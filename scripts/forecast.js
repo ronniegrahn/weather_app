fdbbb59a2682f46eb3247c346bcbee99d110acad
Ronnie Grahn <mista_loco@hotmail.com>
2020-11-16 21:48:43 +0100

    First commit

---
+++ scripts/forecast.js
@@ -0,0 +1,28 @@
+// Working with the api
+class Forecast{
+	constructor() {
+		this.key = "WpNZfr1NRtkXgzZfQo6C2UEhZkONznap";
+		this.weatherURI = "http://dataservice.accuweather.com/currentconditions/v1/";
+		this.cityURI = "http://dataservice.accuweather.com/locations/v1/cities/search";
+	}
+	async updateCity(city){
+		const cityDetails = await this.getCity(city);
+		const weather = await this.getWeather(cityDetails.Key);
+
+		return { cityDetails, weather };
+	}
+	async getCity(city){
+		const query = `?apikey=${this.key}&q=${city}`;
+		const response = await fetch(this.cityURI + query);
+		const data = await response.json();
+
+		return data[0];
+	}
+	async getWeather(id){
+		const query = `${id}?apikey=${this.key}`;
+		const response = await fetch (this.weatherURI + query);
+		const data = await response.json();
+
+		return data[0];
+	}
+}
